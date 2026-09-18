d65d35476442d51a14465cf0ea6e98230cf6c9d8
geng <gengzeshi@51jzcloud.com>
2026-09-18 11:20:53 +0800

    启动文件增加gin服务

--- main.go
+++ main.go
@@ -3,21 +3,25 @@ package main
 import (
 	"go_jichu/conf"
 	inithandle "go_jichu/initHandle"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	configFilePath := "./conf/devConfig.yaml"
 
+	//	加载配置文件
 	cfg, err := conf.LoadConfig(configFilePath)
 	if err != nil {
 		panic(err)
 
 	}
+
+	// 初始化gin环境
 	inithandle.InitGinModel(cfg)
 
-	// // 可以区分线上或者是线下环境
-	// gin.SetMode(gin.DebugMode)
-	// server := gin.Default()
+	// 可以区分线上或者是线下环境
+	server := gin.Default()
 
-	// server.Run(":8080")
+	server.Run(":8080")
 }
