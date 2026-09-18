c9fff0d5fcee50474097d5cd0cd767f2f76ea876
geng <gengzeshi@51jzcloud.com>
2026-09-18 16:31:48 +0800

    抽离响应

---
+++ internal/utils/response/handlerResponse.go
@@ -0,0 +1,34 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	SuccessCode     = 0
+	ParamErrorCode  = 10001
+	ServerErrorCode = 50000
+)
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func Success(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, Response{
+		Code:    0,
+		Message: "success",
+		Data:    data,
+	})
+}
+
+func Error(c *gin.Context, code int, message string) {
+	c.JSON(http.StatusOK, Response{
+		Code:    code,
+		Message: message,
+	})
+}
