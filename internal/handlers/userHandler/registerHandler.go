399c7e800d861b1711d1bbca21f2ca64dd41c2c3
geng <gengzeshi@51jzcloud.com>
2026-09-18 16:43:13 +0800

    记录参数错误日志

--- internal/handlers/userHandler/registerHandler.go
+++ internal/handlers/userHandler/registerHandler.go
@@ -3,7 +3,10 @@ package userhandler
 import (
 	"go_jichu/internal/utils/response"
 
+	utils "go_jichu/internal/utils"
+
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type registerUserStruct struct {
@@ -15,6 +18,10 @@ func RegisterHandler(c *gin.Context) {
 	var registerReq registerUserStruct
 	err := c.ShouldBindJSON(&registerReq)
 	if err != nil {
+		utils.AccessLog.Warn("register function params error",
+			zap.String("path", c.Request.URL.Path),
+			zap.Error(err),
+		)
 		response.Error(c, response.ParamErrorCode, "参数错误")
 		return
 	}
