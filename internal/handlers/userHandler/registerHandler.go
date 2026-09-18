829b53dad1796fe050476d7effe41df887d31b96
geng <gengzeshi@51jzcloud.com>
2026-09-18 16:31:19 +0800

    注册校验参数

--- internal/handlers/userHandler/registerHandler.go
+++ internal/handlers/userHandler/registerHandler.go
@@ -1,9 +1,21 @@
 package userhandler
 
 import (
+	"go_jichu/internal/utils/response"
+
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterHandler(c *gin.Context) {
+type registerUserStruct struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
+func RegisterHandler(c *gin.Context) {
+	var registerReq registerUserStruct
+	err := c.ShouldBindJSON(&registerReq)
+	if err != nil {
+		response.Error(c, response.ParamErrorCode, "参数错误")
+		return
+	}
 }
