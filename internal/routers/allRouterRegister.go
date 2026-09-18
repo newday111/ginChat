4f3862ec9daf209a5dc95fced8e39843a12bf4a1
geng <gengzeshi@51jzcloud.com>
2026-09-18 17:39:18 +0800

    全局注册 邮件路由

--- internal/routers/allRouterRegister.go
+++ internal/routers/allRouterRegister.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	emailrouter "go_jichu/internal/routers/emailRouter"
 	userrouter "go_jichu/internal/routers/userRouter"
 
 	"github.com/gin-gonic/gin"
@@ -8,4 +9,5 @@ import (
 
 func RegisterAllRouter(r *gin.Engine) {
 	userrouter.RegisterUserRouter(r)
+	emailrouter.SendEmailRouter(r)
 }
