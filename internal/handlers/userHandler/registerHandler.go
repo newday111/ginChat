6cd191e7a9d9d072644985c94b4b212d8e6185dc
geng <gengzeshi@51jzcloud.com>
2026-09-21 18:03:53 +0800

    添加注册码以及验证逻辑

--- internal/handlers/userHandler/registerHandler.go
+++ internal/handlers/userHandler/registerHandler.go
@@ -1,18 +1,21 @@
 package userhandler
 
 import (
+	redisdb "go_jichu/internal/db/redisDB"
 	"go_jichu/internal/utils/response"
 
 	utils "go_jichu/internal/utils"
 
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 	"go.uber.org/zap"
 )
 
 type registerUserStruct struct {
-	Username string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
+	Username     string `json:"username" binding:"required"`
+	Password     string `json:"password" binding:"required"`
+	Email        string `json:"email" binding:"required,email"`
+	RegisterCode string `json:"registerCode" binding:"required"`
 }
 
 func RegisterHandler(c *gin.Context) {
@@ -24,6 +27,31 @@ func RegisterHandler(c *gin.Context) {
 			zap.Error(err),
 		)
 		response.Error(c, response.ParamErrorCode, "参数错误")
+		return
+	}
+
+	registerCode, err := redisdb.GlobalRdb.Get(c, registerReq.Email).Result()
+
+	if err == redis.Nil {
+		utils.ErrorLog.Warn("register verification code invalid",
+			zap.String("email", registerReq.Email),
+			zap.Error(err))
+		response.Error(c, response.RegisterVerificationCode, "验证码已失效")
+		return
+	}
+
+	if err != nil {
+		utils.ErrorLog.Warn("register function server error",
+			zap.String("email", registerReq.Email),
+			zap.Error(err))
+		response.Error(c, response.ServerErrorCode, "服务器错误请稍后重试")
+		return
+	}
+
+	if registerCode != registerReq.RegisterCode {
+		utils.ErrorLog.Error("register verification code error",
+			zap.String("对比注册验证码", "注册验证码不一致"))
+		response.Error(c, response.RegisterVerificationCodeNotSame, "验证码不正确")
 		return
 	}
 
